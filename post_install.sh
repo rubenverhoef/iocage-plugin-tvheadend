2edff1f10df026292c41bb0dbd233f256d3d3685
Ruben Verhoef <r.verhoef@cremer.com>
2018-11-02 12:46:00 +0100

    Set default user

--- post_install.sh
+++ post_install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 sysrc 'tvheadend_enable=YES'
-service tvheadend start
+tvheadend -C -f -p /var/run/tvheadend.pid -c /usr/local/etc/tvheadend -u tvheadend -g tvheadend #make default login
+sleep 30
+service tvheadend restart
